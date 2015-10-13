3f64a36bf66807dfc90392de42660a781e9e7a8d
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2015-10-13 01:44:24 -0300

    Create directive  of structure for Birthdate

--- dist/trueform.js
+++ dist/trueform.js
@@ -1,3 +1,27 @@
 /**
  * Created by rodrigo on 13/10/15.
  */
+var trueForm = angular.module("TrueForm", []);
+
+trueForm.directive('tfBirthdate', function(){
+
+  return {
+    restrict: 'A',
+    require: 'ngModel',
+    link: function(scope, attrs, element, ngModel){
+
+      if(ngModel){
+
+        //Parse
+        ngModel.$parses.push(function(value) {
+
+        });
+
+        //Format
+        ngModel.$render = function (value) {
+
+        };
+      }
+    }
+  }
+});
