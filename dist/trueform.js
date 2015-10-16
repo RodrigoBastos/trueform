8fc90035633373e994b3b6247924fff3a4816868
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2015-10-16 13:53:09 -0300

    Keydown Event. Only Numbers in  Date Input

--- dist/trueform.js
+++ dist/trueform.js
@@ -3,90 +3,107 @@
  */
 var trueForm = angular.module("TrueForm", []);
 
-trueForm.directive('tfDate', function($filter){
+var formats  = {
+  'dd/MM/yyyy': {
+    'view': 'dd/MM/yyyy',
+    'delimiter': '/',
+    'regexp': '(0[1-9]|[12][0-9]|3[01])[/.](0[1-9]|1[0-2])[/.]((19|20)[0-9][0-9])'
+  },
+  'dd-MM-yyyy': {
+    'view': 'dd-MM-yyyy',
+    'delimiter': '-',
+    'regexp': '(0[1-9]|[12][0-9]|3[01])[-.](0[1-9]|1[0-2])[-.]((19|20)[0-9][0-9])'
+  },
+  'MM/dd/yyyy': {
+    'view': 'MM/dd/yyyy',
+    'delimiter': '/',
+    'regexp': '(0[1-9]|1[0-2])[/.](0[1-9]|[12][0-9]|3[01])[/.]((19|20)[0-9][0-9])'
+  },
+  'MM-dd-yyyy': {
+    'view': 'MM-dd-yyyy',
+    'delimiter': '-',
+    'regexp': '(0[1-9]|1[0-2])[-.](0[1-9]|[12][0-9]|3[01])[-.]((19|20)[0-9][0-9])'
+  }
+};
+
+trueForm.directive('tfDate', function($filter, $browser){
 
   return {
     restrict: 'A',
     require: 'ngModel',
     link: function(scope, element, attrs, ngModel) {
 
-      var formats  = {
-        'dd/MM/yyyy': {
-          'view': 'dd/MM/yyyy',
-          'delimiter': '/',
-          'regexp': '(0[1-9]|[12][0-9]|3[01])[/.](0[1-9]|1[0-2])[/.]((19|20)[0-9][0-9])'
-        },
-        'dd-MM-yyyy': {
-          'view': 'dd-MM-yyyy',
-          'delimiter': '-',
-          'regexp': '(0[1-9]|[12][0-9]|3[01])[-.](0[1-9]|1[0-2])[-.]((19|20)[0-9][0-9])'
-        },
-        'MM/dd/yyyy': {
-          'view': 'MM/dd/yyyy',
-          'delimiter': '/',
-          'regexp': '(0[1-9]|1[0-2])[/.](0[1-9]|[12][0-9]|3[01])[/.]((19|20)[0-9][0-9])'
-        },
-        'MM-dd-yyyy': {
-          'view': 'MM-dd-yyyy',
-          'delimiter': '-',
-          'regexp': '(0[1-9]|1[0-2])[-.](0[1-9]|[12][0-9]|3[01])[-.]((19|20)[0-9][0-9])'
-        }
+      /*
+      * FUNCTIONS
+      */
+
+      //Event
+      var listener = function () {
+
+        //Current Value
+        var value = element.val();
+
+        //Removes the last character
+        value = value.slice(0, value.length - 1);
+
+        //Update Value
+        element.val(value ? value : '');
+
       };
 
+      //Get Format View
       var getObjectFormatView = function (formatView){
         //Default the format is 'dd/MM/yyyy'
         return formats[formatView] || formats['dd/MM/yyyy'];
       };
 
+      //Rules
+      var isDateValid = function (value, format) {
+
+        var maxDate = attrs.tfDateLimit == 'true';
+
+        var date = value;
+        var year = new Date().getFullYear();
+        var month = new Date().getMonth() + 1; //Jan equals 0
+        var day  = new Date().getDate();
+
+        var error = false;
+        var dateRegExp = new RegExp(format.regexp);
+        var arrayDate = date.split(format.delimiter);
+
+        //RegExp
+        if (date.search(dateRegExp) == -1) error = true;
+
+        //Current Date
+        else if (maxDate && (arrayDate[2] > year || (arrayDate[2] == year && arrayDate[1] > month) || (arrayDate[2] == year && arrayDate[1] == month && arrayDate[0] > day)))
+          error = true;
+
+        //Months with thirty days
+        else if (((arrayDate[1] == 4) || (arrayDate[1] == 6) || (arrayDate[1] == 9) || (arrayDate[1] == 11)) && (arrayDate[0] > 30)) error = true;
+
+        //February
+        else if (arrayDate[1] == 2) {
+          if (arrayDate[0] > 28 && (arrayDate[2] % 4 != 0)) error = true;
+          if (arrayDate[0] > 29 && (arrayDate[2] % 4 != 0)) error = true;
+        }
+
+        return !error;
+
+      };
+
+      //Input Format
+      var editValue = function (oldValue, index, formatCurrent) {
+
+        var lastChar = oldValue[index];
+        var newValue = oldValue;
+
+        if (lastChar != formatCurrent.delimiter) newValue = (oldValue.slice(0,index) + formatCurrent.delimiter + oldValue.slice(index + Math.abs(0)));
+
+        element.val(newValue);
+
+      };
+
       if(ngModel){
-
-        console.log('formatView', attrs.tfDateView);
-
-        //Rules
-        var isDateValid = function (value, format) {
-
-          var maxDate = attrs.tfDateLimit == 'true';
-
-          var date = value;
-          var year = new Date().getFullYear();
-          var month = new Date().getMonth() + 1; //Jan equals 0
-          var day  = new Date().getDate();
-
-          var error = false;
-          var dateRegExp = new RegExp(format.regexp);
-          var arrayDate = date.split(format.delimiter);
-
-          //RegExp
-          if (date.search(dateRegExp) == -1) error = true;
-
-          //Current Date
-          else if (maxDate && (arrayDate[2] > year || (arrayDate[2] == year && arrayDate[1] > month) || (arrayDate[2] == year && arrayDate[1] == month && arrayDate[0] > day)))
-            error = true;
-
-          //Months with thirty days
-          else if (((arrayDate[1] == 4) || (arrayDate[1] == 6) || (arrayDate[1] == 9) || (arrayDate[1] == 11)) && (arrayDate[0] > 30)) error = true;
-
-          //February
-          else if (arrayDate[1] == 2) {
-            if (arrayDate[0] > 28 && (arrayDate[2] % 4 != 0)) error = true;
-            if (arrayDate[0] > 29 && (arrayDate[2] % 4 != 0)) error = true;
-          }
-
-          return !error;
-
-        };
-
-        //Input Format
-        var editValue = function (oldValue, index, formatCurrent) {
-
-          var lastChar = oldValue[index];
-          var newValue = oldValue;
-
-          if (lastChar != formatCurrent.delimiter) newValue = (oldValue.slice(0,index) + formatCurrent.delimiter + oldValue.slice(index + Math.abs(0)));
-
-          element.val(newValue);
-
-        };
 
         //Parse
         ngModel.$parsers.push(function(viewValue) {
@@ -121,6 +138,19 @@ trueForm.directive('tfDate', function($filter){
           element.val($filter('date')(new Date(value), formatCurrent.view));
 
         };
+
+        //Event Keyboard
+        element.bind('keydown', function (event) {
+
+          console.log('event', event);
+          var key = event.keyCode;
+
+          if ((key >= 8 && key <= 57) || (key >= 96 && key <= 105)) return;
+
+
+          $browser.defer(listener);
+
+        });
       }
     }
   };
