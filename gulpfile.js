126a5b62db28488f77c303e6fe6b020e912b5538
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2017-08-08 00:23:19 -0300

    Organiza gulpfile de acordo com o eslint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,13 @@
-var gulp          = require("gulp");
-var rename        = require("gulp-rename");
-var uglify        = require("gulp-uglify");
-var include       = require("gulp-include");
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var include = require('gulp-include');
 
-gulp.task("js", function (done) {
-
-  gulp.src("./source/js/*.js")
+gulp.task('js', function (done) {
+  gulp.src('./source/js/*.js')
     .pipe(include())
-    //.pipe(uglify({mangle:false}))
-    .pipe(rename({extname:".min.js"}))
-    .pipe(gulp.dest("./dist"))
-    .on("end", done);
+    .pipe(uglify({mangle:false}))
+    .pipe(rename({extname:'.min.js'}))
+    .pipe(gulp.dest('./dist'))
+    .on('end', done);
 });
