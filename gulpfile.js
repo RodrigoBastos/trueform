ce11499296cb1ff5c56e2af36de9ece717a23de3
Rodrigo Bastos <rodrigorozendobastos@gmail.com>
2017-08-12 13:34:00 -0300

    Cria função taskJS

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,13 @@ var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var include = require('gulp-include');
 
-gulp.task('js', function (done) {
-  gulp.src('./source/js/*.js')
-    .pipe(include())
-    .pipe(uglify({mangle:false}))
-    .pipe(rename({extname:'.min.js'}))
-    .pipe(gulp.dest('./dist'))
-    .on('end', done);
-});
+function taskJS(done) {
+	gulp.src('./source/js/*.js')
+		.pipe(include())
+		.pipe(uglify({ mangle: false }))
+		.pipe(rename({ extname: '.min.js' }))
+		.pipe(gulp.dest('./dist'))
+		.on('end', done);
+}
+
+gulp.task('js', taskJS);
